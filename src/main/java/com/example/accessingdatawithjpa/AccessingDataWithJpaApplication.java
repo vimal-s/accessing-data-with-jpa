22e2a23dbe600c1e2abcd73d633683cfd9a02320
vimal <vimalsingh99.2@gmail.com>
2019-11-11 15:10:43 +0530

    Add spring boot starter files

---
+++ src/main/java/com/example/accessingdatawithjpa/AccessingDataWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatawithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataWithJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accessingdatawithjpa/AccessingDataWithJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.accessingdatawithjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingDataWithJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
