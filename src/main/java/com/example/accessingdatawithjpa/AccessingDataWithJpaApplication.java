592ff3e59da4b5e2c0840b0c748dbada2705480c
vimal <vimalsingh99.2@gmail.com>
2019-11-11 15:28:00 +0530

    Create repository

--- src/main/java/com/example/accessingdatawithjpa/AccessingDataWithJpaApplication.java
+++ src/main/java/com/example/accessingdatawithjpa/AccessingDataWithJpaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccessingDataWithJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessingDataWithJpaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataWithJpaApplication.class, args);
+    }
 }
